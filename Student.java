82064a849e2676b5306a2054aba1d148cdc797d8
mithun <01fe22bca243@kletech.ac.in>
2024-03-09 09:18:11 -0500

    pushing student program

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public static void main(String[] args) {
+        StudentCourses student01 = new StudentCourses("Mithun", 243);
+        student01.semester01(90, 78);
+        student01.semester02(80, 65);
+        student01.semester03(82,67);
+    }
+}
+
